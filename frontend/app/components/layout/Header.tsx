fd666937e20bf5b1836b2d37c0661704720792e2
wakki0202 <ytk0202@docomo.ne.jp>
2023-06-23 13:20:43 +0900

    first commit

---
+++ frontend/app/components/layout/Header.tsx
@@ -0,0 +1,166 @@
+import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+// ここから変更
+import { Menu as MenuIcon, LocationOn as LocationOnIcon } from '@mui/icons-material';
+// ここまで変更
+
+// ここから変更
+const pages = ['MyList', 'Posts', 'Map'];
+const settings = ['Account', 'Logout'];
+// ここまで変更
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="absolute">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          // ここから変更（アイコンを変更）
+          <LocationOnIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          // ここまで変更
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            // ここから変更 TOGO LIST // ここまで変更
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          // ここから変更（アイコンを変更）
+          <LocationOnIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          // ここまで変更
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            // ここから変更 TOGO LIST // ここまで変更
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button key={page} sx={{ my: 2, color: 'white', fontSize: '15px', display: 'block' }}>
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            // ここから変更
+            <Link color="inherit" underline="none" href="/login">
+              Login
+            </Link>
+            // ここまで変更
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ frontend/app/src/App.tsx
@@ -0,0 +1,26 @@
+import { Box, CssBaseline, Container } from '@mui/material';
+import Header from '../components/layout/Header';
+
+function App() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Header />
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+          pt: { xs: 9, sm: 11, md: 12 },
+        }}
+      >
+        <Container maxWidth="lg">ToGo App</Container>
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
